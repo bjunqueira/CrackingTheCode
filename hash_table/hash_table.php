3c1e5fdc911f58092b036285433612a6c4914cfd
Bruno Junqueira <contas.bj@gmail.com>
2014-06-15 17:31:16 -0300

    Adding misc files and first implementation of hash tables.

---
+++ hash_table/hash_table.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Using PHP we can implement a hash table using a simple associative array.
+ * 
+ * Assumptions: The mapping will be done by a unique Id from the giving elements.
+ * 
+ * @author Bruno Junqueira
+ * @since 2014-06-15
+ */
+
+
+/**
+ * Dummy class to use in the examples.
+ */
+class Element {
+	
+	
+	/**
+	 * Unique Id.
+	 * @var int
+	 */
+	public $id;
+	
+	
+	/**
+	 * The constructor.
+	 * 
+	 * @param int $id The id.
+	 * 
+	 * @return void
+	 */
+	public function __construct($id) {
+		$this->id = $id;
+	}
+
+
+}
+
+
+#
+# First implementation.
+#
+
+
+/**
+ * Build a hash table or insert elements to an existing one if any is given as $hash_table param.
+ *
+ * @param array $Elements   Array of Element objects. 
+ * @param array $hash_table The hash table. If none is given, build it from scratch.
+ *
+ * @return array The hash map.
+ *
+ * @version 1.0
+ */
+function build_table ($Elements, &$hash_table = array())
+{
+	foreach ($Elements as $Element) {
+		$hash_table[$Element->id] = $Element;
+	}
+	return $hash_table;
+}
+
+
+#
+# Improving by adding more OO concepts.
+#
+
+
+/**
+ * The hash table abstraction.
+ * 
+ * @version 2.0
+ */
+class HashTable {
+
+
+	/**
+	 * The hash table itself.
+	 * @var array
+	 */
+	private $table;
+
+
+	/**
+	 * The constructor.
+	 *
+	 * @param array $elements An array of Element objects.
+	 *
+	 * @return void
+	 */
+	public function __construct($elements = array())
+	{
+		$this->table = array();
+		
+		$this->insert($Elements);
+	}
+
+
+	/**
+	 * Build a hash table or insert elements to an existing one if any is given as $hash_table param.
+	 *
+	 * @param array $elements Array of Element objects. 
+	 *
+	 * @return void
+	 */
+	public function insert ($elements)
+	{
+		foreach ($elements as $Element) {
+			$this->table[$Element->id] = $Element;
+		}
+	}
+
+
+	/**
+	 * Fetch element from table.
+	 * 
+	 * @param int $id The element id.
+	 * 
+	 * @return Element
+	 */
+	public function fetch ($id)
+	{
+		return $this->table[$id];
+	}
+
+
+}
+
+
+#
+# Can we improve it? What happens if we pass null as param to insert function?
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
